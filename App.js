359f86b0fbd7e472d20f16324317ba1362f3f742
Winston Meikle <winmeikle@gmail.com>
2020-02-10 18:55:03 -0500

    practice native

---
+++ App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addtoDo';
+import Sandbox from './components/sandbox';
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'practice code', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play smash bros.', key: '3' }
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    });
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      });
+    } else {
+      Alert.alert('Oof', 'Must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
+  }
+
+  return (
+  // <Sandbox />
+
+   <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}> 
+    <View style={styles.container}>
+      {/*header*/}
+      <Header />
+
+    <View style={styles.content}>
+        <AddTodo
+        submitHandler={submitHandler}
+        />
+
+      {/* to form*/}
+      <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+              />
+      </View>
+    </View>
+  </View>
+</TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+    
+  }
+});
